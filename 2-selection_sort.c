3c40dfa74f87cef83ac8d5bcb09061d6421cdd2f
NwoguGlory1 <glorynwogu18@gmail.com>
2023-11-18 08:06:06 -0800

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort -  function that sorts an array of integers
+ * in ascending order using the Selection sort algorithm
+ * @array: pointer to the array
+ * @size: size of the array
+ * Return: ascending order of an array of integers
+ * NIL if size is < 2
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		/*  swap = 0; to show that no swap happens here */
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[i] > array[j])
+			{
+				int temp = array[i];
+
+				array[i] = array[j];
+				array[j] = temp;
+				/* swap = 1; shows that swap happened here */
+				print_array(array, size);
+			}
+		}
+	}
+}
