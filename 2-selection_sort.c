b1b80fe80885bc9652c68ed348f50c5a5503dd02
NwoguGlory1 <glorynwogu18@gmail.com>
2023-11-19 06:10:43 -0800

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,21 +12,28 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i;
 	size_t j;
+	size_t min_value;
 
 	if (size < 2)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
+		min_value = i;
 		/*  swap = 0; to show that no swap happens here */
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[i])
+			if (array[j] < array[min_value])
 			{
+				min_value = j;
+			}
+			if (i != min_value)
+			{
+				/* swap takes place here */
 				int temp = array[i];
 
-				array[i] = array[j];
-				array[j] = temp;
+				array[i] = array[min_value];
+				array[min_value] = temp;
 				/* swap = 1; shows that swap happened here */
 
 				print_array(array, size);
